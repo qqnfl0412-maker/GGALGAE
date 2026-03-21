590e582a11126d5985d30cc43e1f2c0b35d8bd19
qqnfl0412-maker <qqnfl0412@gmail.com>
2026-03-21 15:56:56 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ let hostActionBusy = false;
 
 // Supabase 정보
 const SUPABASE_URL = "https://crzulknhwcvhepajsxnl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2NyenVsa25od2N2aGVwYWpzeG5sLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc3MzUxNjkyNCwiZXhwIjoyMDg5MDkyOTI0fQ.EwbPzEZ4LQMrHxDLEz0MElsAdPj2k9DPXFl_2Kczbyw";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyenVsa25od2N2aGVwYWpzeG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MTY5MjQsImV4cCI6MjA4OTA5MjkyNH0.EwbPzEZ4LQMrHxDLEz0MElsAdPj2k9DPXFl_2Kczbyw";
 
 function pairKey(a, b) {
   return [a, b].sort().join("|");
