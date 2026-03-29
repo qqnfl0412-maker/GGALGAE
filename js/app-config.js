9969fe892720e6f9d6f9cfc82388b52203b0f65b
qqnfl0412-maker <qqnfl0412@gmail.com>
2026-03-29 13:45:49 +0900

    Add files via upload

---
+++ js/app-config.js
@@ -0,0 +1,21 @@
+window.APP_CONFIG = {
+  app: {
+    title: "배드민턴 대진표",
+    splashTitle: "배드민턴 대진 앱",
+    splashSub: "로고는 추후 추가 가능"
+  },
+  game: {
+    maxPlayers: 8,
+    eliminationLosses: 3,
+    targetScore: 25,
+    winBy: 2,
+    courtChangeScore: 11,
+    maxCourts: 2
+  },
+  ui: {
+    splashDelayMs: 1200,
+    splashHideTransitionMs: 400,
+    playerGridColumnsDesktop: 4,
+    playerGridColumnsMobile: 2
+  }
+};
